97594df4694a47343a7f52bdfdcbbf3ee31ed84c
Grant <gkinghorn3@icloud.com>
2023-06-21 15:56:59 +0100

    m

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const path = require("path");
-const fs = require('fs');
+const fs = require("fs");
 
 const express = require("express");
 const app = express();
@@ -7,8 +7,8 @@ const app = express();
 // Set up the view engine for the Express application
 // The first line specifies the directory where the view files are located
 // The second line specifies that the application will use EJS as its view engine
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // allows static files to be served from the defined folder
 app.use(express.static("public"));
@@ -16,24 +16,21 @@ app.use(express.static("public"));
 // parse incoming http request bodies
 app.use(express.urlencoded({ extended: false }));
 
+
 app.get("/", function (req, res) {
-  const htmlFilePath = path.join(__dirname, "views", "index.html");
-  res.sendFile(htmlFilePath);
+  res.render("index");
 });
 
 app.get("/restaurants", function (req, res) {
-  const htmlFilePath = path.join(__dirname, "views", "restaurants.html");
-  res.sendFile(htmlFilePath);
+  res.render("restaurants", { numberOfRestaurants: 2 });
 });
 
 app.get("/confirmation", function (req, res) {
-  htmlFilePath = path.join(__dirname, "views", "confirm.html");
-  res.sendFile(htmlFilePath);
+res.render('confirm')
 });
 
 app.get("/recommend", function (req, res) {
-  htmlFilePath = path.join(__dirname, "views", "recommend.html");
-  res.sendFile(htmlFilePath);
+res.render('recommend');
 });
 
 app.post("/recommend", function (req, res) {
@@ -44,21 +41,18 @@ app.post("/recommend", function (req, res) {
 
   // read the file data
   const fileData = fs.readFileSync(filePath);
-  // convert data to JS object 
-  const storedRestaurants = JSON.parse(fileData)
+  // convert data to JS object
+  const storedRestaurants = JSON.parse(fileData);
   // add form data to the parsed data
   storedRestaurants.push(restaurant);
-  // convert the newly updated data back to JSON and write to defined file 
+  // convert the newly updated data back to JSON and write to defined file
   fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
 
-  res.redirect('/confirmation');
+  res.redirect("/confirmation");
 });
 
 app.get("/about", function (req, res) {
-  htmlFilePath = path.join(__dirname, "views", "about.html");
-  res.sendFile(htmlFilePath);
+res.render('about');
 });
-
-
 
 app.listen(3000);
