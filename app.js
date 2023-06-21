2ca8b0c451caf8f3121a6f9521c7db4cbe21997d
Grant <gkinghorn3@icloud.com>
2023-06-21 11:57:29 +0100

    form post route logic addded

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const path = require("path");
+const fs = require('fs');
 
 const express = require("express");
 const app = express();
 
+// allows static files to be served from the defined folder
 app.use(express.static("public"));
+
+// parse incoming http request bodies
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", function (req, res) {
   const htmlFilePath = path.join(__dirname, "views", "index.html");
@@ -25,9 +30,29 @@ app.get("/recommend", function (req, res) {
   res.sendFile(htmlFilePath);
 });
 
+app.post("/recommend", function (req, res) {
+  // get form data
+  const restaurant = req.body;
+  // define location of file that's being used
+  const filePath = path.join(__dirname, "data", "restaurants.json");
+
+  // read the file data
+  const fileData = fs.readFileSync(filePath);
+  // convert data to JS object 
+  const storedRestaurants = JSON.parse(fileData)
+  // add form data to the parsed data
+  storedRestaurants.push(restaurant);
+  // convert the newly updated data back to JSON and write to defined file 
+  fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+
+  res.redirect('/confirmation');
+});
+
 app.get("/about", function (req, res) {
   htmlFilePath = path.join(__dirname, "views", "about.html");
   res.sendFile(htmlFilePath);
 });
+
+
 
 app.listen(3000);
